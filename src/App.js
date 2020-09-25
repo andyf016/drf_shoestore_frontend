0330f308df2c1e737e298e873d092835d81d40ea
Andrew Fillenwarth <andyf016@gmail.com>
2020-09-25 00:05:57 -0400

    added front end

--- src/App.js
+++ src/App.js
@@ -6,15 +6,31 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      pokemon: [{name: "Dummy"}, {name: "stupid"}]
-    }
+      shoe: [],
+    };
   }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/api/shoe/')
+    .then(res => res.json())
+    .then(data => this.setState({shoe: data}))
+  }
+
   render(){
       return (
     <div >
+      <h1>Shoes</h1>
       <ul>
-      {this.state.pokemon.map(p => (
-        <li>{p.name}</li>
+      {this.state.shoe.map(p => (
+        <div>
+        <li>Brand Name:{p.brand_name}</li>
+        <li>Manufacturer:{p.manufacturer}</li>
+        <li>Color: {p.color}</li>
+        <li>Material: {p.material}</li>
+        <li>Shoe Type: {p.shoe_type}</li>
+        <li>Fasten Type: {p.fasten_type}</li>
+        <br></br>
+        </div>
       ))}
       </ul>
     </div>
